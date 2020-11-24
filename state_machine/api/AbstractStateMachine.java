40ba248dda311055d102d6d807cecf16bfb93b10
Frabal12 <74978115+Frabal12@users.noreply.github.com>
2020-11-24 17:14:08 +0100

    Add files via upload

---
+++ state_machine/api/AbstractStateMachine.java
@@ -0,0 +1,50 @@
+package state_machine.api;
+
+import java.util.List;
+
+import state_machine.core.Event;
+import state_machine.core.State;
+import state_machine.core.Transition;
+
+/**
+ * An abstract class that defines a generic state machine.
+ * 
+ * @param states
+ * @author Davide
+ */
+
+public abstract class AbstractStateMachine {
+	protected List<State> states;
+	protected List<Transition> transitions;
+	protected State currentState;
+
+	/**
+	 * Returns the current state of the machine.
+	 * 
+	 * @return currentState
+	 */
+	public abstract State currentState();
+
+	/**
+	 * Changes the state of the machine based on a specific event.
+	 * 
+	 * @param event
+	 * @throws TransitionException
+	 */
+	public abstract void change(Event event) throws TransitionNotFoundException;
+
+	/**
+	 * Adds a transition to the machine.
+	 * 
+	 * @param transition
+	 */
+	public abstract void addTransition(Transition transition);
+
+	/**
+	 * Checks if a state given as a parameter is the current one.
+	 * 
+	 * @param state
+	 * @return boolean
+	 */
+	public abstract boolean isCurrentState(State state);
+}
